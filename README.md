53d244e60895c167d0472283c72acbf50c580e5e
Jo <yuri@tusion.xyz>
2019-07-29 07:54:59 +0300

    dna

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 
 ## Approach
 
+## C# Learn
+
+1. https://www.tutorialspoint.com/csharp/csharp_decision_making.htm
 
 ## C# Links
 
-
-1. https://repl.it/languages/csharp 
+1. https://repl.it/languages/csharp
 2. https://dotnetcademy.net/CSharp/Beginner
 3. https://codecondo.com/15-resources-to-learn-c-programming-for-beginners/
 4. https://www.learncs.org/
@@ -25,6 +27,40 @@
 - Basic Classes
 - Class Properties
 
+5. https://www.tutlane.com/tutorial/csharp/csharp-value-type-and-reference-type-with-examples
+
+С# basics
+
+6. https://www.tutorialsteacher.com/csharp/csharp-anonymous-type
+
+Cool tutorials for basic usages.
+
+## C# conventions
+
+- Classes, Interfaces and Enums begin with an uppercase letter.
+- Interface names start with an I letter.
+- Comments are placed on a separate line, not at the end of a line of code.
+- Method names begin with a uppercase letter.
+- Only one statement or declaration is placed per line.
+- Identifiers are easily readable and meaningful.
+- Identifiers are written in PascalCase, e.g. each subsequent word begins with a capital letter.
+- The public keyword precedes the static keyword when both are used.
+- The parameter name of the Main() method is called args.
+- Constants are written in uppercase.
+- Curly braces in code blocks start on a new line.
+
+http://zetcode.com/lang/csharp/lexis/
+
+## C# special things
+
+- out int a https://www.tutlane.com/tutorial/csharp/csharp-out-parameter-with-examples
+- ternary operator ?: https://www.tutlane.com/tutorial/csharp/csharp-ternary-operator-with-examples
+- foreach https://www.tutlane.com/tutorial/csharp/csharp-foreach-loop-with-examples
+- do-while https://www.tutlane.com/tutorial/csharp/csharp-do-while-loop-with-examples
+- reference https://www.tutlane.com/tutorial/csharp/csharp-pass-by-reference-ref-with-examples
+- coding conventions https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/inside-a-program/coding-conventions
+
+
 ## MVC links
 
 1. https://www.youtube.com/watch?v=nPrOr1VH_nI
@@ -33,3 +69,7 @@
 # Impactful courses
 
 1. https://academy.yandex.ru/
+
+#  Different resources
+
+1. Algorithms - https://rosettacode.org/wiki/
---
+++ week3/DNAcode/DNAcode/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DNAcode
+{
+    public enum DNACode { A, G, T, C }
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string line;
+            DNACode code;
+
+            Console.Write("Please type DNA sequence (ex: 0102012):");
+            while ((line = Console.ReadLine()) != null)
+            {
+                if (line == "exit")
+                    break;
+            
+                foreach(char c in line)
+                {
+                    code = chr2dnacode(c);
+                    Console.Write( dnacode2symbol(code) );
+                }
+
+                Console.WriteLine("");
+
+                int i;
+                for(i = 0; i< line.Length - 1; i++)
+                {
+                    char c = line[i];
+                    code = chr2dnacode(c);
+                    Console.Write(dnacode2aminobase(code) + "-");
+                }
+
+                if (line.Length > 0)
+                    Console.Write(dnacode2aminobase(chr2dnacode(line[line.Length-1])));
+
+            
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                Console.Write("Please type DNA sequence (ex: 0102012):");
+                //Console.WriteLine(line);
+            }
+
+        }
+
+        static public DNACode chr2dnacode(char c)
+        {
+            switch (c)
+            {
+                case '0': return DNACode.A;
+                case '1': return DNACode.G;
+                case '2': return DNACode.T;
+                case '3': return DNACode.C;
+                default: return DNACode.A; // let it be 'A' for the error
+            }
+        }
+
+
+        static public string dnacode2symbol(DNACode code)
+        {
+            switch (code)
+            {
+                case DNACode.A: return "A";
+                case DNACode.G: return "G";
+                case DNACode.T: return "T";
+                //case DNACode.C: return "C";
+                default: return "C";
+            }            
+        }
+
+        static public string dnacode2aminobase(DNACode code)
+        {
+            switch (code)
+            {
+                case DNACode.A: return "Adenine";
+                case DNACode.G: return "Guanine";
+                case DNACode.T: return "Thymine";
+                //case DNACode.C: return "Cytosine";
+                default: return "Cytosine";
+            }
+        }
+    }
+}
