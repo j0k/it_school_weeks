f0612c24a32d6a925a23113925dd74109122f41f
Jo <yuri@tusion.xyz>
2019-08-07 09:46:15 +0300

    fileupload: Index and Model

---
+++ week4/FileUpload/FileUpload/Models/Picture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace FileUpload.Models
+{
+    public class Picture
+    {
+        public IFormFile File { get; set; }
+    }
+}
